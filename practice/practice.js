46e33aa88df082ded56c575308a3bf27b954e275
Mateo <matthew27garcia@gmail.com>
2022-03-29 23:06:23 -0500

    added the first lines of js but doesn't seem to be calling in the colors nor the heights

---
+++ practice/practice.js
@@ -0,0 +1,46 @@
+// const colorMode = 'dark';
+
+// switch (colorMode) {
+//   case "solarized":
+//    console.log('solarized mode set!'); 
+//    document.body.style.background = 'palegoldenrod';
+//    break;
+//   case 'dark':
+//     console.log('dark mode set!');  
+//     document.body.style.background = 'black';
+//     break;
+//   default:  
+//     console.log('light mode set!');
+//     document.body.style.background = 'ghostwhite';
+//     break;
+// }
+
+// const time = "morning"
+
+// if (time === "morning") {
+//     console.log('good morning')
+// } else if (time === "afternoon") {
+//     console.log('Good afternoon')
+// } else if (time === "night") {
+//     console.log('Goodnight')
+// } else {
+//     console.log('Goodnight')
+// }
+
+
+// const time = ""
+
+// switch (time) {
+//     case "morning":
+//      console.log('Good morning');
+//      break;
+//      case "afternoon":
+//       console.log('Good Afternoon');
+//       break;
+//      case "evening":
+//       console.log('Good Evening')
+//       break;
+//       default: 
+//       console.log('Goodnight')
+// }
+
